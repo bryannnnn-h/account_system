5122f7bdd69c7848d66ae324b48df08efa4674c1
bryannnnn-h <a3108b2413@gmail.com>
2022-08-26 19:09:36 +0800

    edit set fuction on server, sql_connectot 2022/08/26 19:04

--- server/server.py
+++ server/server.py
@@ -1,42 +1,128 @@
-import socketserver, sys, threading
-from time import ctime
-import server_config
-import sql_connecter
+from socketserver import ThreadingMixIn, TCPServer, BaseRequestHandler
+import os, sys, msvcrt, pickle
+import threading
+import logging
+from sql_connector import db_connecter
 
-class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
-    def handle(self):
-        db = sql_connecter.db_connecter()
-        cur = threading.current_thread()
-        print('[%s] Client connected from %s and [%s] is handling with him.' % (ctime(), self.request.getpeername(), cur.name))
-        while True:
-            indata = self.request.recv(1024).strip().decode('utf-8')
-            if len(indata) == 0: 
-                self.request.close()
-                print('client closed connection.')
-                break
-            print(indata)
-            db.dbHandler(indata)
-            self.request.sendall('done'.encode())
-        db.closeDB()
-            
+class MyTCPServer(TCPServer):
+    def __init__(self, server_address, RequestHandler, bind_and_activate=True):
+        super(MyTCPServer, self).__init__(server_address, RequestHandler, bind_and_activate)
+        self.serv_logger = self.createServerLogger()
+        self.db = db_connecter()
+        if self.db.db_error:
+            self.serv_logger.error(f"Fail when connecting to db. Please close and restart server after solving problems.\nPress any keys to close...")
+            msvcrt.getch()
+            self.closeServer(False)
+        self.serv_logger.debug(f'server start at {self.server_address}')
+    
+    def createServerLogger(self):
+        serv_logger: logging.Logger = logging.getLogger(name='server')
+        serv_logger.setLevel(logging.DEBUG)
+        
+        s_handler: logging.StreamHandler = logging.StreamHandler()
+        f_handler: logging.FileHandler = logging.FileHandler(filename='serverLog.log', mode='w')
+        formatter: logging.Formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        s_handler.setFormatter(formatter)
+        f_handler.setFormatter(formatter)
+        serv_logger.addHandler(s_handler)
+        serv_logger.addHandler(f_handler)
+        return serv_logger
+    
+    def closeServer(self,isConnectDB=True):
+        if isConnectDB:
+            self.db.closeDB()
+        self.serv_logger.debug('Server shutdown')
+        self.socket.close()
+        os._exit(0)
 
-class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+
+class ThreadedTCPServer(ThreadingMixIn, MyTCPServer):
     daemon_threads = True
     allow_reuse_address = True
 
+class ThreadedTCPRequestHandler(BaseRequestHandler):
+    def handle(self):
+        db = self.server.db
+        serv_logger = self.server.serv_logger
+        cur = threading.current_thread()
+        try:
+            app = self.request.recv(1024).strip().decode()
+            self.request.sendall('s'.encode())
+            serv_logger.debug(f'connected from {app} and {cur.name} is handling with him.')
+        except Exception as ex:
+            self.request.sendall(str(ex).encode())
+            serv_logger.exception(f'Some errors happen. Please close and restart server after solving problems.\nPress any keys to close...')
+            msvcrt.getch()
+            self.server.closeServer()
+        while True:
+            try:                
+                indata = self.request.recv(1024).strip().decode('utf-8')
+                if len(indata) == 0: 
+                    self.request.close()
+                    serv_logger.debug(f'{app} closed connection.')
+                    serv_logger.debug(f'{cur.name} is closed')
+                    break
+                serv_logger.debug(f'{app} request: {indata}')
+                self.request.sendall('s'.encode())
+                if indata == 'GetData':
+                    indata = self.request.recv(1024).strip().decode('utf-8')
+                    data = db.dbHandler(indata)
+                    if db.db_error:
+                        serv_logger.error(f"[{indata}] by [{app}] fail. Please close and restart server after solving problems.\nPress any keys to close...")
+                        self.request.sendall('fail'.encode())
+                        msvcrt.getch()
+                        self.server.closeServer()
+                    data = pickle.dumps(data)
+                    data_len = len(data)
+                    if data_len == 0:
+                        serv_logger.debug(f'{indata} gets nothing')
+                        self.request.sendall('empty'.encode())
+                        sendflag = self.request.recv(1024).decode()
+                    else:
+                        self.request.sendall(str(data_len).encode())
+                        sendflag = self.request.recv(1024).decode()
+                        if sendflag != 'f':
+                            self.request.sendall(data)
+                            sendflag = self.request.recv(1024).decode()
+                    if sendflag != 's':                       
+                        serv_logger.debug(f'{app} request [{indata}] failed')
+                        self.request.sendall('f'.encode())
+                    else:
+                        serv_logger.debug(f'{app} request [{indata}] done')
+                        self.request.sendall('s'.encode())
 
-if __name__ == '__main__':
-    db = sql_connecter.db_connecter()
-    server = ThreadedTCPServer((server_config.HOST, server_config.PORT), ThreadedTCPRequestHandler)
-    ip, port = server.server_address
-    print('server start at: %s:%s' % (server_config.HOST, server_config.PORT))
+                elif indata == 'SetData':
+                    data_len = self.request.recv(1024).strip().decode()
+                    recv_data = ''
+                    if data_len != 'empty':
+                        self.request.sendall('s'.encode())
+                        try:
+                            data_len = int(data_len)
+                            recv_len = 0
+                            while recv_len < data_len:
+                                data = self.request.recv(1024).decode()
+                                recv_len += len(data)
+                                recv_data += data
+                            self.request.sendall('s'.encode())
+                        except Exception as ex:
+                            self.request.sendall(str(ex).encode())
+                    else:
+                        self.request.sendall('f'.encode())
 
 
-    try:
-        server.serve_forever()
-    except KeyboardInterrupt:
-        sys.exit(0)
+                    db.dbHandler(recv_data)
+                    if db.db_error:
+                        serv_logger.error(f"instruction by [{app}] fail. Please close and restart server after solving problems.\nPress any keys to close...")
+                        self.request.sendall('fail'.encode())
+                        msvcrt.getch()
+                        self.server.closeServer()
+                    serv_logger.debug(f'{app} request instruction done')
+                    self.request.sendall('s'.encode())
+            except:
+                serv_logger.exception(f'Some errors happen. Please close and restart server after solving problems.\nPress any keys to close...')
+                msvcrt.getch()
+                self.server.closeServer()
 
-    
+            
 
 
---
+++ server/sql_connector.py
@@ -0,0 +1,93 @@
+import pymysql
+import logging
+import numpy as np
+
+db_settings = {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "user": "root",
+    "password": "bayern2000",
+    "charset": "utf8mb4"
+}
+
+class db_connecter:
+    def __init__(self):
+        self.db_error = None
+        self.conn = None
+        self.db_logger, self.db_error_logger = self.createDBLogger()
+        self.DBconnection()
+
+    def DBconnection(self):
+        try:
+            self.conn = pymysql.connect(**db_settings)
+            try:
+                self.db_cursor = self.conn.cursor()
+                self.db_cursor.execute('CREATE DATABASE IF NOT EXISTS ernies_db DEFAULT CHARACTER SET utf8mb4')
+                self.db_cursor.execute('USE ernies_db')
+                self.db_cursor.execute('CREATE TABLE IF NOT EXISTS basic_info (id INT, name VARCHAR(20), grade INT, program VARCHAR(20), price INT)')
+                self.db_cursor.execute('CREATE TABLE IF NOT EXISTS TodayMenu (StoreName VARCHAR(20), ItemName VARCHAR(20), price INT)')
+                self.db_cursor.execute('CREATE TABLE IF NOT EXISTS TodayRecord (StoreName VARCHAR(20),StudentName VARCHAR(20), ItemName VARCHAR(20), price INT, amount INT, TotalPrice INT)')
+                self.db_cursor.execute('CREATE TABLE IF NOT EXISTS HistoryRecord (date VARCHAR(20), time VARCHAR(20), StoreName VARCHAR(20), StudentName VARCHAR(20), ItemName VARCHAR(20), price INT, amount INT, TotalPrice INT)')
+                self.db_cursor.execute('CREATE TABLE IF NOT EXISTS FavMenu (StoreName VARCHAR(20), FavMenuName VARCHAR(20), ItemName VARCHAR(20), price INT)')
+            except Exception as ex:
+                self.closeDB()
+                self.db_error = ex
+                self.db_error_logger.exception("Catch an exception when creating database or tables")
+        except Exception as ex:
+            self.db_error = ex
+            self.db_error_logger.exception("Catch an exception when connecting to database")
+
+    def closeDB(self):
+        self.db_logger.debug('Closing database')
+        self.conn.close()
+    
+    def createDBLogger(self):
+        db_logger: logging.Logger = logging.getLogger(name='db')
+        db_logger.setLevel(logging.DEBUG)
+        db_error_logger: logging.Logger = logging.getLogger(name='dbError')
+        db_error_logger.setLevel(logging.WARNING)
+        
+        formatter: logging.Formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        s_handler: logging.StreamHandler = logging.StreamHandler()
+        f_handler: logging.FileHandler = logging.FileHandler(filename='dbLog.log', mode='w')
+        s_handler.setFormatter(formatter)
+        f_handler.setFormatter(formatter)
+        db_logger.addHandler(s_handler)
+        db_logger.addHandler(f_handler)
+
+        f_error_handler: logging.FileHandler = logging.FileHandler(filename='dbErrorLog.log', mode='w')
+        f_error_handler.setFormatter(formatter)
+        db_error_logger.addHandler(s_handler)
+        db_error_logger.addHandler(f_error_handler)
+        return db_logger, db_error_logger
+
+    def dbHandler(self, instruction):
+        self.db_logger.debug(f'db execute {instruction}')
+        instrcution_label = instruction.split(" ")[0]
+        instruction_content = ' '.join(instruction.split(" ")[1:])
+        try:
+            if instrcution_label == "set": 
+                setTable, setcol, setValue = instruction_content.split(' ') 
+                sqlCommand = f'INSERT INTO {setTable} {setcol} VALUES {setValue}'[:-1]
+                
+                print(sqlCommand)
+                self.db_cursor.execute(sqlCommand)
+                self.conn.commit()
+
+            elif instrcution_label == "Fetch":
+                fetch_table = instruction_content.split(' ')[0]
+                fetch_column = ','.join(instruction_content.split(' ')[1:])
+                self.db_cursor.execute(f'SELECT {fetch_column} FROM {fetch_table}')
+                result = np.array(self.db_cursor.fetchall())
+                return result
+
+            elif instrcution_label == "Clear":
+                clearTable = instruction_content
+                self.db_cursor.execute("TRUNCATE TABLE %s" % (clearTable))
+                self.conn.commit()
+            
+            self.db_logger.debug(f'{instruction} success')
+        except Exception as ex:
+            self.db_error = ex
+            self.db_error_logger.exception(f"Catch an exception when {instruction})")
