f5ab772bbb6dd6117f683ef6fead32a2c35184e6
bryannnnn-h <a3108b2413@gmail.com>
2022-09-28 22:37:53 +0800

    2022/09/28

--- server/sql_connector.py
+++ server/sql_connector.py
@@ -97,7 +97,7 @@ class db_connecter:
                         fetch_condition_msg += cond
                         if i != fetch_condition_list[-1]:
                             fetch_condition_msg += ' AND '
-                print(f'SELECT {fetch_column} FROM {fetch_table}{fetch_condition_msg}')
+                
                 self.db_cursor.execute(f'SELECT {fetch_column} FROM {fetch_table}{fetch_condition_msg}')
                 result = np.array(self.db_cursor.fetchall())
                 return result
